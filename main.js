6785867a9523dbc51299a84067c5c18b18ca629e
SpitSmoke <jmfelicio.sp@gmail.com>
2023-12-06 01:50:49 -0300

    insercao de java

--- main.js
+++ main.js
@@ -1,10 +1,55 @@
-$(document).ready(function(){
+$(document).ready(function() {
     $('#carousel-imagens').slick({
         autoplay: true,
         autoplaySpeed: 2000,
     });
 
-$('.menu-hamburguer').click(function (){
-    $('nav').slideToggle();
-})
-})
+    $('.menu-hamburguer').click(function () {
+        $('nav').slideToggle();
+    });
+
+    $('#nome').mask('', {
+        placeholder: 'Ebac Motors da Silva'
+    });
+
+    $('#telefone').mask('(00) 00000-0000', {
+        placeholder: '(00) 00000-0000'
+    });
+
+
+$('form').validate({
+    rules: {
+        nome: {
+            required: true
+        },
+        email: {
+            required: true, 
+            email: true
+        },
+        telefone: {
+            required: true
+        },
+        mensagem: {
+            required: true,
+        },
+        veiculoDeInteresse: {
+            required: false,
+        }
+    },
+    messages: {
+        nome:'Insira seu nome',
+        email:'Insira um email valido',
+        telefone:'Insira um telefone'
+    },
+    submitHandler: function(form) {
+        console.log(form);
+    },
+    invalidHandler: function(evento, validador) { 
+        let camposIncorretos = validador.numberOfInvalids(); 
+        if (camposIncorretos) {
+            alert(`Existem ${camposIncorretos} campos incorretos`);
+        }
+    }
+});
+
+});
